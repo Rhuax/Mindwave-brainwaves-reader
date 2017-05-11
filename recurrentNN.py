bdc598a2b3bcdce369a4ca1a85f21890c03157a1
Stefn93 <stefan.romanazzi@gmail.com>
2017-05-11 15:44:22 +0200

    Damn 2.7 wtf dude

--- recurrentNN.py
+++ recurrentNN.py
@@ -26,7 +26,7 @@ def read_eeg_dataset(ds_path, max_sequence_length):
             if lines >= max_sequence_length:
                 break
 
-    print "Data loaded from csv. Formatting..."
+    print("Data loaded from csv. Formatting...")
 
     result = []
     for index in range(len(data_array) - sequence_length):
@@ -35,8 +35,8 @@ def read_eeg_dataset(ds_path, max_sequence_length):
 
     result_mean = result.mean()
     result -= result_mean
-    print "Shift : ", result_mean
-    print "Data  : ", result.shape
+    print("Shift : ", result_mean)
+    print("Data  : ", result.shape)
 
     row = int(round(0.9 * result.shape[0]))
     train = result[:row, :]
@@ -73,7 +73,7 @@ def build_model():
 
     start = time.time()
     model.compile(loss="mse", optimizer="rmsprop")
-    print "Compilation Time : ", time.time() - start
+    print("Compilation Time : ", time.time() - start)
     return model
 
 
@@ -85,13 +85,13 @@ def run_network(model=None, data=None):
     path_to_dataset = 'household_data_array_consumption.txt'
 
     if data is None:
-        print 'Loading data... '
+        print('Loading data... ')
         X_train, y_train, X_test, y_test = read_eeg_dataset(
             path_to_dataset, sequence_length, ratio)
     else:
         X_train, y_train, X_test, y_test = data
 
-    print '\nData Loaded. Compiling...\n'
+    print('\nData Loaded. Compiling...\n')
 
     if model is None:
         model = build_model()
@@ -103,7 +103,7 @@ def run_network(model=None, data=None):
         predicted = model.predict(X_test)
         predicted = np.reshape(predicted, (predicted.size,))
     except KeyboardInterrupt:
-        print 'Training duration (s) : ', time.time() - global_start_time
+        print('Training duration (s) : ', time.time() - global_start_time)
         return model, y_test, 0
 
     try:
@@ -113,8 +113,8 @@ def run_network(model=None, data=None):
         plt.plot(predicted[:100])
         plt.show()
     except Exception as e:
-        print str(e)
-    print 'Training duration (s) : ', time.time() - global_start_time
+        print(str(e))
+    print('Training duration (s) : ', time.time() - global_start_time)
 
     return model, y_test, predicted
 
