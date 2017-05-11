8d4cea3782c2d1822ace6a9971a29dfd30066127
crow <gelidcrow@gmail.com>
2017-05-11 15:36:18 +0200

    Fixed bugs

--- training.py
+++ training.py
@@ -3,11 +3,7 @@ import numpy as np
 #from keras.layers import LSTM
 
 
-dataset=np.genfromtxt('ioio.csv',delimiter=',',dtype=np.int32)
-
-
-dataset = np.genfromtxt('ioio.csv',delimiter=',',dtype=np.int32)
-
+dataset=np.genfromtxt('eegdataset.csv',delimiter=',',dtype=np.int32)
 
 def calculate_max_sequence_length(dataset):
     max_seq_length = 0
@@ -30,12 +26,11 @@ def calculate_max_sequence_length(dataset):
 
 def create_array_task(dataset):
     current_output = dataset[0][-4:]
-    array_task = np.zeros(51)
+    array_task = np.zeros(52)
     line = 0
-    i = 0
+    i = 1
     for row in dataset:
         o = row[-4:]
-
         if not np.array_equal(o, current_output):
             array_task[i] = line
             i += 1
@@ -43,7 +38,7 @@ def create_array_task(dataset):
         line += 1
     return array_task
 
-#print(calculate_max_sequence_length(dataset))
+print(calculate_max_sequence_length(dataset))
 
 """
 It builds the network, defining its structure
