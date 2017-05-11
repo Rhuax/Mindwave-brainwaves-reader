e996c8ad5b0f1c6c6b467941d1c8b2a505e4397d
Stefn93 <stefan.romanazzi@gmail.com>
2017-05-11 13:33:09 +0200

    arrayTask
    function to calculate array with line numbers of different tasks

--- training.py
+++ training.py
@@ -1,26 +1,39 @@
 import numpy as np
-dataset=np.genfromtxt('ioio.csv',delimiter=',',dtype=np.int32)
 
-
+dataset = np.genfromtxt('ioio.csv',delimiter=',',dtype=np.int32)
 
 
 def calculate_max_sequence_length(dataset):
-    max_seq_length=0
-    cur_seq_length=0
-    current_output=dataset[0][-4:]
-    seqs=1
+    max_seq_length = 0
+    cur_seq_length = 0
+    current_output = dataset[0][-4:]
+    seqs = 1
     for row in dataset:
         o=row[-4:]
-        if not np.array_equal(o,current_output):
-            seqs+=1
-            if cur_seq_length>max_seq_length:
+        if not np.array_equal(o, current_output):
+            seqs += 1
+            if cur_seq_length > max_seq_length:
 
-                max_seq_length=cur_seq_length
-            current_output=o
-            cur_seq_length=1
+                max_seq_length = cur_seq_length
+            current_output = o
+            cur_seq_length = 1
         else:
-            cur_seq_length+=1
-    return max_seq_length,seqs
+            cur_seq_length += 1
+    return max_seq_length, seqs
 
+
+def create_array_task(dataset):
+    current_output = dataset[0][-4:]
+    array_task = []
+    line = 0
+    i = 0
+    for row in dataset:
+        o = row[-4:]
+        line += 1
+        if not np.array_equal(o, current_output):
+            array_task[i] = line
+            i += 1
+    return array_task
 
 print(calculate_max_sequence_length(dataset))
+print(create_array_task(dataset))
