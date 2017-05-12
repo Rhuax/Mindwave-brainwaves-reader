491f6116f60303794810ef73b71ffd2677a3935c
crow <gelidcrow@gmail.com>
2017-05-12 21:49:33 +0200

    Logica per il calcolo dell'accuratezza su test set e altre robe belle

--- training.py
+++ training.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-from keras.layers import LSTM,Dense,Dropout
+from keras.layers import LSTM, Dense, Dropout
 from keras.models import Sequential
+from keras.optimizers import RMSprop, SGD, Adam
 
 np.set_printoptions(linewidth=200)
-epochs=1
-dataset=np.genfromtxt('eegdataset.csv',delimiter=',',dtype=np.int32)
+epochs = 10
+dataset = np.genfromtxt('eegdataset.csv', delimiter=',', dtype=np.int32)
 
 
 def calculate_max_sequence_length(dataset):
@@ -14,11 +15,10 @@ def calculate_max_sequence_length(dataset):
     current_output = dataset[0][-4:]
     seqs = 1
     for row in dataset:
-        o=row[-4:]
+        o = row[-4:]
         if not np.array_equal(o, current_output):
             seqs += 1
             if cur_seq_length > max_seq_length:
-
                 max_seq_length = cur_seq_length
             current_output = o
             cur_seq_length = 1
@@ -29,7 +29,7 @@ def calculate_max_sequence_length(dataset):
 
 def create_array_task(dataset, sequences):
     current_output = dataset[0][-4:]
-    array_task = np.zeros(sequences,dtype=np.int32)
+    array_task = np.zeros(sequences, dtype=np.int32)
     line = 0
     i = 1
     for row in dataset:
@@ -42,74 +42,124 @@ def create_array_task(dataset, sequences):
         line += 1
     return array_task
 
+
 def k_fold_CV(dataset, folds):
     folds += 1
-    fold_len = int(len(dataset)/folds)
-    train_all = [[0 for x in range(folds)] for y in range(len(dataset)-fold_len)]
+    fold_len = int(len(dataset) / folds)
+    train_all = [[0 for x in range(folds)] for y in range(len(dataset) - fold_len)]
     validate_all = [[0 for x in range(folds)] for y in range(fold_len)]
-    for i in range(1,folds):
-        val_start = i*fold_len
-        val_end = (i+1)*fold_len
-        train_all[i][:val_start] = dataset[:val_start-1]
-        train_all[i][val_start+1:] = dataset[val_end+1:]
+    for i in range(1, folds):
+        val_start = i * fold_len
+        val_end = (i + 1) * fold_len
+        train_all[i][:val_start] = dataset[:val_start - 1]
+        train_all[i][val_start + 1:] = dataset[val_end + 1:]
         validate_all[i] = dataset[val_start:val_end]
     return train_all, validate_all
 
+
 train, val = k_fold_CV(dataset, 10)
 for i in range(1, 11):
-    np.savetxt('cross_validation/training_'+str(i)+'.csv', train[i], fmt='%i', delimiter=',')
-    np.savetxt('cross_validation/testing_'+str(i)+'.csv', val[i], fmt='%i', delimiter=',')
-
+    np.savetxt('cross_validation/training_' + str(i) + '.csv', train[i], fmt='%i', delimiter=',')
+    np.savetxt('cross_validation/testing_' + str(i) + '.csv', val[i], fmt='%i', delimiter=',')
 
 """
 It builds the network, defining its structure
 """
+
+
 def create_model():
-    model=Sequential()
-    model.add(LSTM(11,stateful=True,return_sequences=True,batch_input_shape=(1,5,11)))
-    model.add(LSTM(11,return_sequences=True))
-    model.add(Dropout(0.3))
-    model.add(LSTM(11))
-    model.add(Dense(4,activation='softmax'))
+    model = Sequential()
+    model.add(LSTM(11, stateful=True, return_sequences=True, batch_input_shape=(1, 5, 11)))
+    model.add(LSTM(11, return_sequences=True))
+    # model.add(Dropout(.2))
+    model.add(LSTM(11, return_sequences=True))
+    # model.add(Dropout(.2))
+    model.add(LSTM(6))
+    model.add(Dense(4, activation='softmax'))
     return model
 
 
+def calculate_accuracy(set, model):
+    m, n_test_sequences = calculate_max_sequence_length(set)
+    indices = create_array_task(set, n_test_sequences)
+    current_sequence_test = 0
+    total_accuracy=0
+    for seqa in range(n_test_sequences - 1):
+        starta = indices[current_sequence_test]
+        enda = indices[current_sequence_test + 1]
 
+        y_true = np.reshape(set[starta, -4:], (1, 4))
+        #print('y è :', end='')
+        #print(y_true)
 
-max_length,sequences=calculate_max_sequence_length(dataset)
-sequences_indices=create_array_task(dataset,sequences=sequences)
+        batch_x=np.expand_dims(set[starta:enda,0:-4],0)
 
+        k = 0
+        true_positives=0
+        while k < (enda - starta) / 5:
+            batch_x = np.expand_dims(set[starta+k * batch_size:starta+((k + 1) * batch_size), 0:-4],0)
+            output=model.predict_on_batch(batch_x)
+            r_output=np.zeros((1,4))
+            r_output[0][np.argmax(output)]=1
+            if np.array_equal(r_output,y_true):
+                true_positives+=1
 
-batch_size=5
-network=create_model()
+            k += 1
+        print('accuracy : ',end='')
+        total_accuracy+=true_positives/((enda-starta)/5)
+        model.reset_states()
+        current_sequence_test+=1
+    print('accuratezza sul test set: ',end='')
+    print(total_accuracy/n_test_sequences)
+
+max_length, sequences = calculate_max_sequence_length(dataset)
+sequences_indices = create_array_task(dataset, sequences=sequences)
+
+test_set = dataset[sequences_indices[-8]:]
+dataset = dataset[:sequences_indices[-8]]
+
+max_length, sequences = calculate_max_sequence_length(dataset)
+
+batch_size = 5
+network = create_model()
 print('Compiling model..')
-network.compile(optimizer='rmsprop',loss='categorical_crossentropy')
+opt = RMSprop(lr=0.0005)
+network.compile(optimizer=opt, loss='categorical_crossentropy',
+                metrics=['accuracy'])
 print('Model compiled. Specs:')
 network.summary()
+
 for e in range(epochs):
-    current_sequence=0
-    for seq in range(sequences-1):
-        print('sequenza'+str(current_sequence))
-        start=sequences_indices[current_sequence]
-        end=sequences_indices[current_sequence+1]
-        Train=dataset[start:end]
-        y_true=np.reshape(Train[0][-4:],(1,4))
-        j=0
-        err=0
-        while(j<(end-start)/5):
-            batch_x = Train[j * batch_size:(j + 1) * batch_size,0:-4]
+    current_sequence = 0
+    epoch_accuracy = 0
+    for seq in range(sequences - 1):
+        # print('sequenza'+str(current_sequence))
+        start = sequences_indices[current_sequence]
+        end = sequences_indices[current_sequence + 1]
+        Train = dataset[start:end]
+        y_true = np.reshape(Train[0][-4:], (1, 4))
+        j = 0
+        err = 0
+        acc = 0
+        while (j < (end - start) / 5):
+            batch_x = Train[j * batch_size:(j + 1) * batch_size, 0:-4]
             batch_x = np.expand_dims(batch_x, 0)
-            loss=network.train_on_batch(batch_x,y_true)
-            err+=loss
+            loss, accuracy = network.train_on_batch(batch_x, y_true)
+            # print('Accuracy: '+str(accuracy))
+            err += loss
+            acc += accuracy
             """print('loss on batch '+str(j),end=' ')
             print(':'+str(loss))"""
-            j+=1
-        print('errore medio per questa sequenza '+str(err/((end-start)/5)))
+            j += 1
+        # print('errore medio per questa sequenza '+str(err/((end-start)/5)))
+        epoch_accuracy += acc / ((end - start) / 5)
+        # print('accuratezza media per questa sequenza '+str(acc/((end-start)/5)))
         network.reset_states()
-        current_sequence+=1
+        current_sequence += 1
+    print('accuratezza media per ques\'epoca sul training set: ' + str(epoch_accuracy / sequences))
+    calculate_accuracy(test_set,network)
 
-f=open('architecture','w')
+f = open('architecture', 'w')
 f.write(network.to_json())
 f.close()
 network.save('network.h5')
-
