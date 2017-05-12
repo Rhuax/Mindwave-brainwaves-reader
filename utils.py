ab45efa3735515bc34ee5ea85f26adff4e2a34d2
Stefn93 <stefan.romanazzi@gmail.com>
2017-05-12 18:08:30 +0200

    fix 1

--- utils.py
+++ utils.py
@@ -18,22 +18,25 @@ def create_array_task(dataset, sequences):
         line += 1
     return array_task
 
-def two_over_thirteen_fold_CV(dataset):
+def two_over_thirteen_fold_CV(dataset, increment, test_people):
+    seq_step = 4
+    i = 0
+
     seqs = create_array_task(dataset, 52)
     train_all = [[-1 for x in range(11)] for y in range(len(dataset))]
     validate_all = [[-1 for x in range(11)] for y in range(len(dataset))]
-    i = 0
-    while i < 11:
+
+    while i < (11 * seq_step):
         val_start = seqs[i]
-        val_end = seqs[i+2]
-        train_all[i][:val_start] = dataset[:val_start - 1]
-        train_all[i][val_start + 1:] = dataset[val_end + 1:]
-        validate_all[i] = dataset[val_start:val_end]
-        i += 1
+        val_end = seqs[i + (test_people * 4)]
+        train_all[int(i/(seq_step * increment))][:val_start] = dataset[:val_start - 1]
+        train_all[int(i/(seq_step * increment))][val_start + 1:] = dataset[val_end + 1:]
+        validate_all[int(i/(seq_step * increment))] = dataset[val_start:val_end]
+        i += (seq_step*increment)
     return train_all, validate_all
 
 
-train, val = two_over_thirteen_fold_CV(dataset)
+train, val = two_over_thirteen_fold_CV(dataset, 1, 2)
 for i in range(11):
     np.savetxt('cross_validation/training_'+str(i)+'.csv', train[i], fmt='%i', delimiter=',')
     np.savetxt('cross_validation/testing_'+str(i)+'.csv', val[i], fmt='%i', delimiter=',')
