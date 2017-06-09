0c7491159eccb41242ac2d4f39e8c58124c28b9c
Stefano Romanazzi <stefan.romanazzi@gmail.com>
2017-06-09 21:18:51 +0200

    Almost correlated

--- signalCorrelation.py
+++ signalCorrelation.py
@@ -9,19 +9,31 @@ tasks = ['memoria', 'logica', 'rilassamento', 'musica_metal']
 waves = ['delta', 'theta', 'lowAlpha', 'highAlpha', 'lowBeta', 'highBeta', 'lowGamma', 'midGamma']
 
 means = []
+corrMat = []
+
 
 
 def mean_wave(i_wave, squared_waves):
     s = np.sum(squared_waves[:, i_wave])
-    return np.sqrt(s)
+    s = np.sqrt(s)
+    return s.astype(int)
 
+
+def correlate_waves(person, wave_1, wave_2):
+    for filename in os.listdir('records/by_person/' + person):
+        dataset = np.genfromtxt('records/by_person/' + filename, delimiter=',', dtype=int)
+        dataset = dataset[:, :-4]
+        datasets.append(dataset)
+        print(datasets)
+    for i in np.shape(datasets)[0]:
+        corrMat[wave_1][wave_2] += datasets[i][wave_1]*datasets[i][wave_2] / means[wave_1] * means[wave_2]
 
 total_waves = np.zeros((0, 8))
 
 for task in tasks:
     ds = np.zeros((0, 8))
-    datasets = []
     dims = []
+    datasets = []
     for filename in os.listdir('records/by_task/' + task):
         dataset = np.genfromtxt('records/by_task/' + task + '/' + filename, delimiter=',', dtype=int)
         dataset = dataset[:, :-4]
@@ -33,7 +45,7 @@ for task in tasks:
         # corr(np.min(dims), ds)
 
 squared_waves = np.square(total_waves)
-for i in range(waves.__len__()):
+for i in range(np.shape(waves)[0]):
     means.append(mean_wave(i, squared_waves))
 
 print(means)
