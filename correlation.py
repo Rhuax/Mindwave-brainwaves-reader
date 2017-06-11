5007254f164a17c18dff832746ceca6d093fc6c2
Gianlucator <gianluca.principini@gmail.com>
2017-06-11 14:54:53 +0200

    correlazioni per task e persone con csv

--- correlation.py
+++ correlation.py
@@ -7,9 +7,10 @@ import pylab
 import seaborn as sns
 
 np.set_printoptions(precision=7, suppress=True)
+
 tasks = ['memoria', 'logica', 'rilassamento', 'musica_metal']
 waves = ['delta', 'theta', 'lowAlpha', 'highAlpha', 'lowBeta', 'highBeta', 'lowGamma', 'midGamma']
-
+persons = ['angelo', 'asia', 'claudio', 'emanuele', 'fabiola', 'gianluca', 'giulia', 'michel', 'milad', 'mirella', 'monica', 'roberta', 'stefano']
 
 def mean(wave):
     return np.sqrt(np.sum(np.square(wave)))
@@ -31,20 +32,55 @@ def correlate(mean1, mean2, wave1, wave2):
 
 for task in tasks:
     dims = []
+    complete_ds = np.zeros((0, 8))
     for filename in os.listdir('records/by_task/' + task):
         print(filename)
         dataset = np.genfromtxt('records/by_task/' + task + '/' + filename, delimiter=',', dtype=np.int)
         dataset = dataset[:, :-4]
         dims.append(np.shape(dataset)[0])
     mindim = np.min(dims)
+    means = []
     for filename in os.listdir('records/by_task/' + task):
-        means = []
         dataset = np.genfromtxt('records/by_task/' + task + '/' + filename, delimiter=',', dtype=np.int)
         dataset = dataset[:mindim, :-4]
-        for i in range(waves.__len__()):
-            means.append(mean(dataset[:, i]))
-        sns.plt.figure(figsize=(12, 9))
-        sns.heatmap(correlation(dataset, means))
-        sns.plt.title(filename)
-        sns.plt.savefig('plots/correlation/' + filename + '.svg', dpi=100)
-        sns.plt.clf()
+        complete_ds = np.vstack([complete_ds, dataset])
+
+    for i in range(waves.__len__()):
+        means.append(mean(complete_ds[:, i]))
+    sns.plt.figure(figsize=(12, 9))
+    corr = correlation(complete_ds, means)
+    sns.heatmap(corr)
+    sns.plt.title(task)
+    np.savetxt('plots/correlation_by_task/'+task+'_dataset.csv', complete_ds, fmt = '%1.10f')
+    np.savetxt('plots/correlation_by_task/'+task+'_means.csv', np.array(means), fmt = '%1.10f')
+    np.savetxt('plots/correlation_by_task/'+task+'_correlation.csv', corr, fmt = '%1.10f')
+
+    sns.plt.savefig('plots/correlation_by_task/' + task + '.svg', dpi=100)
+    sns.plt.clf()
+
+for person in persons:
+    dims = []
+    complete_ds = np.zeros((0, 8))
+    for filename in os.listdir('records/by_person/' + person):
+        print(filename)
+        dataset = np.genfromtxt('records/by_person/' + person + '/' + filename, delimiter=',', dtype=np.int)
+        dataset = dataset[:, :-4]
+        dims.append(np.shape(dataset)[0])
+    mindim = np.min(dims)
+    means = []
+    for filename in os.listdir('records/by_person/' + person):
+        dataset = np.genfromtxt('records/by_person/' + person + '/' + filename, delimiter=',', dtype=np.int)
+        dataset = dataset[:mindim, :-4]
+        complete_ds = np.vstack([complete_ds, dataset])
+    for i in range(waves.__len__()):
+        means.append(mean(complete_ds[:, i]))
+    sns.plt.figure(figsize=(12, 9))
+    corr = correlation(complete_ds, means)
+    sns.heatmap(corr)
+    sns.plt.title(person)
+    np.savetxt('plots/correlation_by_person/'+person+'_dataset.csv', complete_ds, fmt = '%1.10f')
+    np.savetxt('plots/correlation_by_person/'+person+'_means.csv', np.array(means), fmt = '%1.10f')
+    np.savetxt('plots/correlation_by_person/'+person+'_correlation.csv', corr, fmt = '%1.10f')
+
+    sns.plt.savefig('plots/correlation_by_person/' + person + '.svg', dpi=100)
+    sns.plt.clf()
